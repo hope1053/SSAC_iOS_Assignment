783c1ccfaf4753fbf36ad978f02e54b4c50a69dd
hyerin <hope1053@naver.com>
2021-09-30 14:33:33 +0900

    [210930] UI 배치

--- SSAC_Day4_SignUp/SSAC_Day4_SignUp/SignUpViewController.swift
+++ SSAC_Day4_SignUp/SSAC_Day4_SignUp/SignUpViewController.swift
@@ -15,15 +15,6 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // 코드로 placeholder 색 바꾸기
+    // 버튼 corner radius 변경하기
 }
