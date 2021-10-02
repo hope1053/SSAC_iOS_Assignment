585596accab3f4bbaec090eb0ba3e80b802a9bf7
hyerin <hope1053@naver.com>
2021-10-02 14:00:31 +0900

    [211002] 파일 생성 & Device Orientation 설정

---
+++ SSAC_Day5_LEDBoard/SSAC_Day5_LEDBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_Day5_LEDBoard
+//
+//  Created by 최혜린 on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
