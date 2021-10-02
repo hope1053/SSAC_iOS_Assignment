800974dd641b598efc3b3fdfe8f178a9d2208424
hyerin <hope1053@naver.com>
2021-10-02 14:37:50 +0900

    [211002] Send & Color Butotn 구현

--- SSAC_Day5_LEDBoard/SSAC_Day5_LEDBoard/ViewController.swift
+++ SSAC_Day5_LEDBoard/SSAC_Day5_LEDBoard/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var boardView: UIView!
+    @IBOutlet var writtenText: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sendTextButton(_ sender: UIButton) {
+        guard let boardText = writtenText.text else {return}
+        resultLabel.text = boardText
+    }
+    
+    @IBAction func changeColorButton(_ sender: UIButton) {
+        let randomColorArray: [UIColor] = [.red, .yellow, .white, .green, .blue, .lightGray, .magenta, .orange, .purple]
+        resultLabel.textColor = randomColorArray.randomElement()
+    }
+    
 }
 
