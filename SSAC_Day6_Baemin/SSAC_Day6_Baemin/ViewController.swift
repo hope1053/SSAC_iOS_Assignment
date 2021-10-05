423016a242b8fb227d3e54def88e639430872cb1
hyerin <hope1053@naver.com>
2021-10-05 15:15:25 +0900

    [211005] UI 구성

---
+++ SSAC_Day6_Baemin/SSAC_Day6_Baemin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_Day6_Baemin
+//
+//  Created by 최혜린 on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
