85ac6ce22a5a638d9cf4d015cb94b8906e3a6c3e
hyerin <hope1053@naver.com>
2021-10-06 14:55:10 +0900

    [211006] 파일 생성

---
+++ SSAC_Day7_EmtionDiary/SSAC_Day7_EmtionDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_Day7_EmtionDiary
+//
+//  Created by 최혜린 on 2021/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
