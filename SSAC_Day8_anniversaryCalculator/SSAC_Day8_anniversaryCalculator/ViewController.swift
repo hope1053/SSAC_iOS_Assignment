af73239fac276d23ba2f8e1589e81167e88a7c65
hyerin <hope1053@naver.com>
2021-10-07 17:27:05 +0900

    [211007] 파일 생성

---
+++ SSAC_Day8_anniversaryCalculator/SSAC_Day8_anniversaryCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSAC_Day8_anniversaryCalculator
+//
+//  Created by 최혜린 on 2021/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var datePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
