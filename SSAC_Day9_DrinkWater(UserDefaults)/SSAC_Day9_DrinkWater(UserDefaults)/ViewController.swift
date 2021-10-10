4d10ad2d90dc121f591553e2510d42d1783211d8
hyerin <hope1053@naver.com>
2021-10-10 23:06:50 +0900

    [211010] 파일 생성

---
+++ SSAC_Day9_DrinkWater(UserDefaults)/SSAC_Day9_DrinkWater(UserDefaults)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_Day9_DrinkWater(UserDefaults)
+//
+//  Created by 최혜린 on 2021/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
