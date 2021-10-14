9d9f5221eabf2cad95ed312cf5bef2972a98c80c
hyerin <hope1053@naver.com>
2021-10-14 20:02:09 +0900

    [211014] 환율계산기 코드

---
+++ SSAC_Day11_ExchangeRate.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+struct ExchangeRate {
+    var currencyRate: Double {
+        willSet {
+            print("currencyRate willSet - 환율 변동 예정: \(currencyRate) -> \(newValue)")
+        }
+        didSet {
+            print("currencyRate didSet - 환율 변동 완료: \(oldValue) -> \(currencyRate)")
+        }
+    }
+    
+    var USD: Double {
+        willSet {
+            print("USD willSEt - 환전 금액: USD: \(newValue)달러로 환전될 예정")
+        }
+        didSet {
+            print("USD didSet - KRW: \(KRW)원 -> \(USD)달러로 환전되었음")
+        }
+    }
+    
+    var KRW: Double {
+        get {
+            return USD * currencyRate
+        }
+        set {
+            USD = newValue / currencyRate
+        }
+    }
+}
+
+var rate = ExchangeRate(currencyRate: 1100, USD: 1)
+rate.KRW = 500000
+rate.currencyRate = 1350
+rate.KRW = 500000
