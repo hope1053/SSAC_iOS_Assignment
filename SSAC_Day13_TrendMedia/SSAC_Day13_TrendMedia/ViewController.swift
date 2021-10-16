4bd9d5392bbed1fcfd26437ca84a0ac310d9fad8
hyerin <hope1053@naver.com>
2021-10-17 00:00:01 +0900

    [211016] 파일 생성

---
+++ SSAC_Day13_TrendMedia/SSAC_Day13_TrendMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_Day13_TrendMedia
+//
+//  Created by 최혜린 on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
