5cffd7f55b44c1a66734dbf58b231cdac5fb46d0
hyerin <hope1053@naver.com>
2021-10-16 23:52:36 +0900

    [211016] Struct 구성 및 코드 리팩토링

---
+++ SSAC_Day12_ShoppingList/SSAC_Day12_ShoppingList/Shopping.swift
@@ -0,0 +1,14 @@
+//
+//  Shopping.swift
+//  SSAC_Day12_ShoppingList
+//
+//  Created by 최혜린 on 2021/10/16.
+//
+
+import Foundation
+
+struct Shopping {
+    let whatToBuy: String
+    var checkTapped: Bool
+    var starTapped: Bool
+}
