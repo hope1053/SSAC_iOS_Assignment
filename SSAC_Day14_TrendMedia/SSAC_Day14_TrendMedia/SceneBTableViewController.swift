12cd205d133254e4854f8206715a55b1a0f8f3ea
hyerin <hope1053@naver.com>
2021-10-18 15:18:31 +0900

    커밋메시지

---
+++ SSAC_Day14_TrendMedia/SSAC_Day14_TrendMedia/SceneBTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SceneTableViewController.swift
+//  SSAC_Day13_TrendMedia
+//
+//  Created by 최혜린 on 2021/10/17.
+//
+
+import UIKit
+
+class SceneBTableViewController: UITableViewController {
+    let tvShowInformation = TvShowInformation()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "영화 검색"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(closeButtonClicked))
+    }
+    
+    @objc func closeButtonClicked() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tvShowInformation.tvShow.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SceneBCell", for: indexPath) as? SceneBTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let data = tvShowInformation.tvShow[indexPath.row]
+        cell.posterImageView.image = UIImage(named: data.title)
+        cell.titleLabel.text = data.title
+        cell.releaseDate.text = "\(data.releaseDate)"
+        cell.overview.text = data.overview
+        
+        return cell
+    }
+}
---
+++ SSAC_Day14_TrendMedia/SSAC_Day14_TrendMedia/SceneCTableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SceneCTableViewController.swift
+//  SSAC_Day13_TrendMedia
+//
+//  Created by 최혜린 on 2021/10/17.
+//
+
+import UIKit
+import Kingfisher
+
+class SceneCTableViewController: UITableViewController {
+    
+    var currentTvShow: TvShow?
+    var starring: String = ""
+    var starringList: [String] = []
+    var tvShowName: String?
+    
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var headerPosterImageView: UIImageView!
+    @IBOutlet var headerTitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "출연/제작"
+        starring = currentTvShow!.starring
+        tvShowName = currentTvShow?.title
+        starringList = starring.components(separatedBy: ", ")
+        updateUI()
+    }
+    
+    func updateUI() {
+        headerPosterImageView.image = UIImage(named: tvShowName!)
+        headerTitleLabel.text = tvShowName!
+        let url = URL(string: currentTvShow!.backdropImage)
+        headerImageView.kf.setImage(with: url)
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return starringList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SceneCCell", for: indexPath) as? SceneCTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.nameLabel.text = starringList[indexPath.row]
+        cell.castImageView.image = UIImage(named: tvShowName!)
+        cell.castImageView.layer.cornerRadius = 10
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UIScreen.main.bounds.height / 10
+    }
+    
+//    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 10
+//    }
+}
